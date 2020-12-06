07775d4c34601d9516c5ad0984112ed4a7a6f6cd
“teji1998” <tejasvinirpk@gmail.com>
2020-12-06 20:44:51 +0530

    Added:Test case and code to find maximum of three float numbers at first position

--- maximumTest/FindMaximum.cs
+++ maximumTest/FindMaximum.cs
@@ -6,9 +6,19 @@ namespace maximumTest
 {
     public class FindMaximum
     {
-        public int FindingMaximum(int firstValue, int secondValue, int thirdValue) 
+        public int FindingMaximumIntegerValue(int firstValue, int secondValue, int thirdValue) 
         {
             int maximum = firstValue;
+            if (secondValue.CompareTo(maximum) > 0)
+                maximum = secondValue;
+            if (thirdValue.CompareTo(maximum) > 0)
+                maximum = thirdValue;
+            return maximum;
+        }
+
+        public float FindingMaximumFloatValue(float firstValue, float secondValue, float thirdValue)
+        {
+            float maximum = firstValue;
             if (secondValue.CompareTo(maximum) > 0)
                 maximum = secondValue;
             if (thirdValue.CompareTo(maximum) > 0)
--- maximumTestUsingMSTest/UnitTest1.cs
+++ maximumTestUsingMSTest/UnitTest1.cs
@@ -17,22 +17,31 @@ namespace maximumTestUsingMSTest
         [TestMethod]
         public void givenThreeIntegers_WhenMaximumIntegerAtFirstPosition_ShouldReturnMaximumInteger()
         {
-            int maximumInteger = findMaximum.FindingMaximum(8, 3, 5);
+            int maximumInteger = findMaximum.FindingMaximumIntegerValue(8, 3, 5);
             Assert.AreEqual(8, maximumInteger);
         }
 
         [TestMethod]
         public void givenThreeIntegers_WhenMaximumIntegerAtSecondPosition_ShouldReturnMaximumInteger()
         {
-            int maximumInteger = findMaximum.FindingMaximum(3, 8, 5);
+            int maximumInteger = findMaximum.FindingMaximumIntegerValue(3, 8, 5);
             Assert.AreEqual(8, maximumInteger);
         }
 
         [TestMethod]
         public void givenThreeIntegers_WhenMaximumIntegerAtThirdPosition_ShouldReturnMaximumInteger()
         {
-            int maximumInteger = findMaximum.FindingMaximum(3, 5, 8);
+            int maximumInteger = findMaximum.FindingMaximumIntegerValue(3, 5, 8);
             Assert.AreEqual(8, maximumInteger);
         }
+
+        [TestMethod]
+        public void givenThreeIntegers_WhenMaximumFloatNumberAtFirstPosition_ShouldReturnMaximumFloatNumber()
+        {
+            float maximumValue = findMaximum.FindingMaximumFloatValue(37.5f, 56.4f, 87.6f);
+            Assert.AreEqual(87.6f, maximumValue);
+        }
+
+
     }
 }
