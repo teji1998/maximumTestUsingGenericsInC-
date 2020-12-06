cef631a5134c866bb55cd45f76e6a8bce9da9eba
“teji1998” <tejasvinirpk@gmail.com>
2020-12-06 19:19:39 +0530

    Added:Test case to find maximum of three numbers at third position

--- maximumTestUsingMSTest/UnitTest1.cs
+++ maximumTestUsingMSTest/UnitTest1.cs
@@ -27,5 +27,12 @@ namespace maximumTestUsingMSTest
             int maximumInteger = findMaximum.FindingMaximum(3, 8, 5);
             Assert.AreEqual(8, maximumInteger);
         }
+
+        [TestMethod]
+        public void givenThreeIntegers_WhenMaximumIntegerAtThirdPosition_ShouldReturnMaximumInteger()
+        {
+            int maximumInteger = findMaximum.FindingMaximum(3, 5, 8);
+            Assert.AreEqual(8, maximumInteger);
+        }
     }
 }
