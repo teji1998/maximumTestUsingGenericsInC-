5dddd25f38347406a0fc99f4f5c764647be5f1b5
“teji1998” <tejasvinirpk@gmail.com>
2020-12-06 16:53:34 +0530

    Added:Welcome message

---
+++ maximumTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace maximumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the maximum test problem using generics!!!");
+        }
+    }
+}
